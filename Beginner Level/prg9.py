679c3e8c7e67f3a1fcf3438cc676667f6fbf1bc0
PreethamKumar14 <37337325+PreethamKumar14@users.noreply.github.com>
2018-03-21 06:41:26 +0000

    Update prg9.py

--- Beginner Level/prg9.py
+++ Beginner Level/prg9.py
@@ -1,9 +1,9 @@
-n=int(input())
-k=int(input())
+n=int(raw_input())
+k=int(raw_input())
 sum=0
 arr=[0]*n
 for i in range(0,n):
-    arr[i]=int(input())
+    arr[i]=int(raw_input())
 for j in range(0,k):
     sum=sum+arr[j]
 print(sum)
