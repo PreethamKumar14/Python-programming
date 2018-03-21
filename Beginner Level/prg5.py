c18b12eba19d9bac27075a0939d4c97797d309e9
PreethamKumar14 <37337325+PreethamKumar14@users.noreply.github.com>
2018-03-21 04:37:53 +0000

    Update prg5.py

--- Beginner Level/prg5.py
+++ Beginner Level/prg5.py
@@ -4,14 +4,14 @@ b=input()
 c=input()
 if isinstance(a,str) or isinstance(b,str) or isinstance(c,str):
     if(a>='a' and a<='z' and b>='a' and b<='z' and c>='a' and c<='z'):
-		print("Invalid Input")
+        print("Invalid Input")
+    else:
+        a=int(a)
+	b=int(b)
+	c=int(c)
+	if(a>b and a>c):
+	    print(a)
+        elif(b>a and b>c):
+	    print(b)
 	else:
-	    a=int(a)
-		  b=int(b)
-		  c=int(c)
-		  if(a>b and a>c):
-			    print(a)
-		  elif(b>a and b>c):
-			    print(b)
-		  else:
-			    print(c)
+	    print(c)
