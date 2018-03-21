9b72318097d45878e381e8724fd2a7bb07154afa
PreethamKumar14 <37337325+PreethamKumar14@users.noreply.github.com>
2018-03-21 05:32:54 +0000

    Update prg12.py

--- Beginner Level/prg12.py
+++ Beginner Level/prg12.py
@@ -1,13 +1,13 @@
 a=int(raw_input())
 if(a<=1000):
     temp=a
-    x=0
-        while(temp!=0):
-            x=x*10+(temp%10)
-            temp=(temp/10)
-    if(x==a):
-        print("Palindrome")
-    else:
-        print("Not Palindrome")
+	x=0
+	while(temp!=0):
+        x=x*10+(temp%10)
+        temp=(temp/10)
+	if(x==a):
+	  	print("Palindrome")
+	else:
+		print("Not Palindrome")
 else:
-    print("no")
+	print("no")
