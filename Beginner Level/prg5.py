aef9890625f6de19af4fefca5b36e07df984f04b
PreethamKumar14 <37337325+PreethamKumar14@users.noreply.github.com>
2018-03-21 04:43:08 +0000

    Update prg5.py

--- Beginner Level/prg5.py
+++ Beginner Level/prg5.py
@@ -1,17 +1,16 @@
-print("Enter 3 numbers:")
-a=input()
-b=input()
-c=input()
+a=raw_input()
+b=raw_input()
+c=raw_input()
 if isinstance(a,str) or isinstance(b,str) or isinstance(c,str):
     if(a>='a' and a<='z' and b>='a' and b<='z' and c>='a' and c<='z'):
         print("Invalid Input")
     else:
         a=int(a)
-	b=int(b)
-	c=int(c)
-	if(a>b and a>c):
-	    print(a)
+        b=int(b)
+        c=int(c)
+        if(a>b and a>c):
+            print(a)
         elif(b>a and b>c):
-	    print(b)
-	else:
-	    print(c)
+            print(b)
+        else:
+            print(c)
